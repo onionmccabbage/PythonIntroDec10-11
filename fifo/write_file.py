49028dcea75266fa22046ea343278a4a555f8722
onionmccabbage <onionmccabbage@live.com>
2024-12-10 15:52:18 +0000

    printing and writing for text file (including 'with' and try-except)

--- fifo/write_file.py
+++ fifo/write_file.py
@@ -5,12 +5,25 @@ def printToFile():
     # if we use 'xt' it will only print to file if it does NOT already exist
     try: # try lets us write an exception handler in case of problems
         fout = open('my_log.txt', 'at') # this asks the operating system to access the file system
+        # NB print always puts a new-line character at the end
         print('hello from Python', file=fout)
+        # its a good idea to close a file access object to release resources
+        fout.close()
     except FileExistsError as err:
         print(f'There was a problem: {err}')
 
 def writeToFile():
-    '''more control over that gets written into a persistent text file'''
+    '''more control over what gets written into a persistent text file'''
+    try:
+        fout = open('file.txt', 'at')
+        # 'with' will close the file access object for us ( no need for fout.close() )
+        with fout: # this is a tidy way to handle file access
+            # .write does NOT put a new line character at the end
+            fout.write('this is persisted into a text file\n') # we may choose to add a new line character
+            # also \t for tab
+    except Exception as err: # here we handle ANY exception
+        print(f'Problem: {err}')
 
 # here we exercise the code
 printToFile() # this will create the file if it does not yet exist, otherwise append to it
+writeToFile()
