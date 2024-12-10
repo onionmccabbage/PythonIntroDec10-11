e2e537a8b2f6e4e3ad75d6ffa85f4e6ca01e1bf0
onionmccabbage <onionmccabbage@live.com>
2024-12-10 16:03:30 +0000

    read and readlines for text

--- fifo/read_file.py
+++ fifo/read_file.py
@@ -3,10 +3,14 @@
 def readText():
     '''Retrieve any text from a specific text file
     return the entire contents of the text file'''
-    fin = open('file.txt', 'rt') # 'rt' means read text
-    with fin: # ...this will close the file access object when done
-        retrieved = fin.read()
-    return retrieved
+    try: # always good to use try-except when dealing with external assets
+        fin = open('file.txt', 'rt') # 'rt' means read text
+        with fin: # ...this will close the file access object when done
+            # retrieved = fin.read() # read() will retrieve the entire file contents
+            retrieved = fin.readlines() # retrieves the entire file into a LIST of single lines
+        return retrieved
+    except Exception as err:
+        print(f'Something went wrong: {err}')
 
 # exercise the code
 r = readText()
