3d42524b8829c74d3d4c55dc6de76d5419717d2b
onionmccabbage <onionmccabbage@live.com>
2024-12-10 16:26:38 +0000

    working with CSV and data structures

--- fifo/using_csv.py
+++ fifo/using_csv.py
@@ -19,6 +19,35 @@ def fileWriter(t):
         except Exception as err:
             print(f'{err}')
 
-# read back all the text into a list of lines
+# fileWriter(h)
+# fileWriter(j)
+# fileWriter(k)
 
-# itersate over this retrieved text to populate a list of dictionaries
+# read back all the text into a list of lines
+def readCSV():
+    '''retrieve all the contents of a CSV text file
+    into a list where each member is a line'''
+    try:
+        fin = open('names.csv', 'rt')
+        with fin:
+            r = fin.readlines() # retreive a list of all the lines
+        return r
+    except Exception as err:
+        print(err)
+
+c = readCSV()
+print(c) # expect a list of lines
+
+# iterate over this retrieved text to populate a list of dictionaries
+def makeListOfDict(l):
+    '''iterate over the list, making a dict object for each user
+    Append each new suer to a list'''
+    users = [] # an empty list
+    for i in l: # iterate of the the list 'l' 
+        user = i.split(', ')
+        d = {'name':user[0], 'age':user[1], 'level':user[2], 'skill':user[3]}
+        users.append(d) # we have a list of dictionaries
+    return users
+
+r = makeListOfDict(c)
+print(r)
