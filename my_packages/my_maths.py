029f0f57ede1ad85ce94804f5b0c9fda4f40d81c
onionmccabbage <onionmccabbage@live.com>
2024-12-10 15:20:06 +0000

    using docstring (triple quotes)

--- my_packages/my_maths.py
+++ my_packages/my_maths.py
@@ -1,7 +1,9 @@
 # here we will do some clever calculations
 def addThem(x, y):
-    # return the sum of x+y
+    '''return the sum of x+y'''
     return x+y
 
 def subtractThem(a,b):
+    '''We often write a DOCSTRING to explain what were up to
+    This is a good place to document a function'''
     return a-b
