a11fbaafe891744887eb11647683f6c6f4aa7a6c
onionmccabbage <onionmccabbage@live.com>
2024-12-10 12:19:44 +0000

    invoking a function many times

--- using_functions.py
+++ using_functions.py
@@ -1,6 +1,6 @@
 # functions let us encapsulate code so we can reuse it whenever we need
-# if we wish me may pass in arguments to a function
-def makeCubic(x): # we define a function in a code block
+# if we wish me may pass in arguments to a function (optionally include a sensible default)
+def makeCubic(x=1): # we define a function in a code block
     # a good idea to check we are dealing with a number
     if(type(x)==int or type(x)==float):
         return x*x*x # the function will stop when it runs 'return' (or if there are no more lines)
@@ -10,3 +10,9 @@ def makeCubic(x): # we define a function in a code block
 # we then use the function like this
 answer = makeCubic(4.2) # the () invoke the function so it runs
 print(answer)
+# we can print the result of a function ccall directly
+print( makeCubic(4) )
+print( makeCubic(-6) )
+print( makeCubic(31452) )
+print( makeCubic(0) )
+print( makeCubic('wibbly') ) # fails nicely
