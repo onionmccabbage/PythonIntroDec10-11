b1457362cc8e48130cadfda4928f9aa2710e1132
onionmccabbage <onionmccabbage@live.com>
2024-12-10 11:53:35 +0000

    using range

--- data_types.py
+++ data_types.py
@@ -23,3 +23,16 @@ for i in n: # iterate over the range
     print(i)
 
 # the 'range' is a really-efficient tool for large quantities of values
+# e.g. we may need a bunch of even numbers
+e = range(-10, 11, 2) #(start, stop-before, step)
+g =input('please enter a number: ') # input is always a string
+if g.isnumeric():
+    # did they enter an even number?
+    if int(g) in e:
+        print(f'{g} is an even number between -10 and 10')
+
+# we ca nuse a range to create a load of calculations
+# we may need some square numbers 1-10
+squares = [i*i for i in range(1,11)] # remember [] will make a list
+print(squares)
+    
